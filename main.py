b0cf632c4179ea4a781b6c476fa0ce6c94a4d396
Andoni <aagirre92@gmail.com>
2022-06-03 16:51:12 +0200

    Readme updated

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from functions import process_complexity
 import config as cfg
 
 if __name__ == '__main__':
-    cr_instance = ReportGenerator(control_room_url=cfg.cr_url, username=cfg.username, api_key=cfg.apiKey)
-    # cr_instance = ReportGenerator(control_room_url=cfg.cr_url, username=cfg.username, password=cfg.password)
+    # cr_instance = ReportGenerator(control_room_url=cfg.cr_url, username=cfg.username, api_key=cfg.apiKey)
+    cr_instance = ReportGenerator(control_room_url=cfg.cr_url, username=cfg.username_support_user,
+                                  api_key=cfg.apiKey_support_user)
     df = cr_instance.get_bot_full_reports("3669")
     overall_complexity = process_complexity(df)
-    print("Overall complexity: "+str(overall_complexity))
-
+    print("Overall complexity: " + str(overall_complexity))
